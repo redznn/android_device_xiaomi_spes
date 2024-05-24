6da9ffa4149a2183b9e7c07158991ece41ab09db
Bruno Martins <bgcngm@gmail.com>
2024-05-24 20:37:51 -0300

    spes: Use common libqti-perfd-client and power-libperfmgr
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -242,6 +242,7 @@ USE_SENSOR_MULTI_HAL := true
 BOARD_VNDK_VERSION := current
 
 # Sepolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 ifdef CR_VERSION
